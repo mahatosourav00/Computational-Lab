608e41c7993a5dcca34567c861a9b6311abcfdb0
SOURAV MAHATO <42140479+mahatosourav00@users.noreply.github.com>
2020-08-08 01:29:23 +0530

    Update 2nd question.py

--- First assignment/Python/2nd question.py
+++ First assignment/Python/2nd question.py
@@ -4,7 +4,7 @@ integer = int(input("Enter the integer for which you want calculate factorial = 
 if integer < 0:
     while integer < 0:
         integer = int(input("The integer must be > or = to 0 = "))
-        backup = integer
+backup = integer
 while (integer > 0):
     multiply =multiply*integer
     integer=integer-1
