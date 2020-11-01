1a249a6563a11e5b53b667ab73bc676426d5bcd9
SOURAV MAHATO <42140479+mahatosourav00@users.noreply.github.com>
2020-11-01 20:56:19 +0530

    Add files via upload

--- Assignment 6/Q4_Ass6.py
+++ Assignment 6/Q4_Ass6.py
@@ -13,11 +13,14 @@ b = 1
 #no. of random values
 N = 100000
 
+#set the time for how long the monte carlo fuction will run(more time means more no. of N)
+duration = 4        #The funtion will run for 4 seconds
+
 #calling function
-intg = my_library.monte_carlo(a, b, fun, N, file)
+intg = my_library.monte_carlo(a, b, fun, duration, file)
 print("Integration of the function = ", intg)
 
 ## -:The Exact Output appended below:-##
 '''
-Integration of the function =  3.1416513746167505
+Integration of the function =  3.1415730638082224
 '''
